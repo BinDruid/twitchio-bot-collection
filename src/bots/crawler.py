b7ed9bbc081de7ec8c527e2a81365978c0c2f109
BinDruid <abharya.dev@gmail.com>
2023-01-17 00:48:01 +0330

    feat: crawl bot saves emotes, emojis, mentions and their counts for each message fix: crawl bot was not ignoring user messages starting with prefix insall: add emoji package

--- src/bots/crawler.py
+++ src/bots/crawler.py
@@ -6,11 +6,17 @@ class Crawler(DataBaseProcessMixin, CommonBot):
     async def event_ready(self):
         await super().event_ready()
         await self.init_database_routine()
+        print("Bot is listening to chat events!")
+
+    async def get_prefix(self, message):
+        """
+        Forcing crawl bot to ignore user commands started with prefix
+        """
+        return None
 
     async def event_message(self, message):
         if message.echo:
             return
-
         context = await self.get_context(message)
 
         if context.is_reward_redemption:
@@ -19,7 +25,5 @@ class Crawler(DataBaseProcessMixin, CommonBot):
         self.save_context(context)
 
     def save_context(self, context: AdvancedContext):
-        username = context.message.tags["display-name"]
-        message = context.message.content
-        self.insert_into_messages(username, message)
-        print(f"{username}: {message}")
+        self.insert_into_chats_table(context)
+        print(f"{context.author['name']}: {context.message.content}")
--- src/mixins/database.py
+++ src/mixins/database.py
@@ -1,11 +1,13 @@
 import os
+import emoji
 import psycopg2
 import requests
 
 
 class DataBaseProcessMixin:
+    _emotes = None
     _conn_string = os.environ["DB_URL"]
-    _providers = ["twitch", "7tv", "bttv", "ffz"]
+    _emotes_providers = ["twitch", "7tv", "bttv", "ffz"]
     _channel_emotes_endpoint = f"https://emotes.adamcy.pl/v1/channel/{os.environ['CHANNEL']}/emotes/twitch.7tv.bttv.ffz"
     _global_emotes_endpoint = (
         "https://emotes.adamcy.pl/v1/global/emotes/twitch.7tv.bttv.ffz"
@@ -25,24 +27,28 @@ class DataBaseProcessMixin:
         return wrapper
 
     async def init_database_routine(self):
+        self._create_chats_table()
         self._create_emotes_table()
-        self._create_messages_table()
+        self._create_message_emojis_table()
+        self._create_message_emotes_table()
+        self._create_message_mentions_table()
         self._update_emotes()
+        self._make_emote_list()
 
     def _update_emotes(self):
-        print("Updating global emote database")
-        self._get_emotes(self._global_emotes_endpoint, "global")
-        print("Updating channel emote database")
-        self._get_emotes(self._channel_emotes_endpoint, "channel")
+        print("Updating database for global emotes")
+        self._fetch_emotes(self._global_emotes_endpoint, "global")
+        print("Updating database for channel emotes")
+        self._fetch_emotes(self._channel_emotes_endpoint, "channel")
 
-    def _get_emotes(self, url, set):
+    def _fetch_emotes(self, url, set):
         response = requests.get(url)
         data = response.json()
         for emote in data:
-            self._insert_into_emotes(emote, set)
+            self._insert_into_emotes_table(emote, set)
 
     @connect_database
-    def _insert_into_emotes(self, emote, set, cursor):
+    def _insert_into_emotes_table(self, emote, set, cursor):
         exist_query = """select exists
                         (select code from emotes where code=%s);"""
         cursor.execute(exist_query, (emote["code"],))
@@ -56,33 +62,102 @@ class DataBaseProcessMixin:
                 (
                     emote["code"],
                     emote["urls"][0]["url"],
-                    self._providers[emote["provider"]],
+                    self._emotes_providers[emote["provider"]],
                     set,
                 ),
             )
             print(f"New emote added: {emote['code']}")
 
     @connect_database
-    def insert_into_messages(self, user, message, cursor):
-        insert_query = """insert into chat_messages
-                        (username, message)
-                        values (%s, %s);"""
+    def insert_into_chats_table(self, context, cursor):
+        message = context.message.content
+        author = context.author
+        emotes_count = self._count_emotes(message)
+        emojis_count = self._count_emojis(message)
+        mentions_count = self._count_mentions(message)
+        insert_query = """insert into chats
+                        (message, username, user_id, is_sub, is_mod, is_vip)
+                        values (%s, %s, %s, %s, %s, %s) returning id;"""
         cursor.execute(
             insert_query,
             (
-                user,
                 message,
+                author.name,
+                author.id,
+                author.is_subscriber,
+                author.is_mod,
+                author.is_vip,
             ),
+        )
+        message_id = cursor.fetchone()[0]
+        emote_insert_query = """insert into message_emotes
+                            (message_id, emote_id, count)
+                            values (%s, %s, %s);"""
+        for emote in emotes_count:
+            cursor.execute(
+                emote_insert_query,
+                (message_id, self._emotes[emote], emotes_count[emote]),
+            )
+        emoji_insert_query = """insert into message_emojis
+                            (message_id, emoji, count)
+                            values (%s, %s, %s);"""
+        for emj in emojis_count:
+            cursor.execute(
+                emoji_insert_query,
+                (message_id, emj, emojis_count[emj]),
+            )
+
+        mention_insert_query = """insert into message_mentions
+                            (message_id, mention, count)
+                            values (%s, %s, %s);"""
+        for mention in mentions_count:
+            cursor.execute(
+                mention_insert_query,
+                (message_id, mention, mentions_count[mention]),
+            )
+
+    @connect_database
+    def _create_chats_table(self, cursor):
+        cursor.execute(
+            """create table if not exists chats(
+            id serial primary key,
+            message varchar(600),
+            username varchar(255),
+            user_id integer,
+            is_sub boolean default false,
+            is_mod boolean default false,
+            is_vip boolean default false,
+            date timestamptz default (now() at time zone 'Iran'))"""
         )
 
     @connect_database
-    def _create_messages_table(self, cursor):
+    def _create_message_mentions_table(self, cursor):
         cursor.execute(
-            """create table if not exists chat_messages(
+            """create table if not exists message_mentions(
             id serial primary key,
-            username varchar(255),
-            message varchar(600),
-            date timestamptz default (now() at time zone 'Iran'))"""
+            message_id integer references chats(id) not null,
+            mention varchar(255),
+            count integer)"""
+        )
+
+    @connect_database
+    def _create_message_emojis_table(self, cursor):
+        cursor.execute(
+            """create table if not exists message_emojis(
+            id serial primary key,
+            message_id integer references chats(id) not null,
+            emoji varchar(255),
+            count integer)"""
+        )
+
+    @connect_database
+    def _create_message_emotes_table(self, cursor):
+        cursor.execute(
+            """create table if not exists message_emotes(
+            id serial primary key,
+            message_id integer references chats(id) not null,
+            emote_id integer references emotes(id) not null,
+            count integer)"""
         )
 
     @connect_database
@@ -96,3 +171,25 @@ class DataBaseProcessMixin:
             set varchar(10),
             date timestamptz default (now() at time zone 'Iran'))"""
         )
+
+    @connect_database
+    def _make_emote_list(self, cursor):
+        cursor.execute("""select id, code from emotes;""")
+        emotes = cursor.fetchall()
+        # emote[1] is emote code
+        # emote[0] is id of the emote
+        self._emotes = {emote[1]: emote[0] for emote in emotes}
+
+    def _count_emojis(self, message):
+        emojis = [word for word in message.split(" ") if emoji.is_emoji(word)]
+        return {emj: emojis.count(emj) for emj in set(emojis)}
+
+    def _count_emotes(self, message):
+        emotes = [word for word in message.split(" ") if word in self._emotes]
+        return {emote: emotes.count(emote) for emote in set(emotes)}
+
+    def _count_mentions(self, message):
+        mentions = [
+            word.replace(",", "") for word in message.split(" ") if word.startswith("@")
+        ]
+        return {mention: mentions.count(mention) for mention in set(mentions)}
